3bd1ab8c46f25b3e2facc75a16b04ea271b5a3f7
Daniel Hudson <DanielHudson@users.noreply.github.com>
2023-05-12 22:27:20 +0100

    Update HeroBackground.jsx

--- app/component/HeroBackground.jsx
+++ app/component/HeroBackground.jsx
@@ -5,7 +5,7 @@ export default function HeroBackground({children, className, image}) {
   return (
     <section className={classNames(className)}>
       <div className="relative mt-8 bg-gradient-to-r lg:bg-gradient-to-l from-sea-pink-500 to-transparent">
-        <div className="px-8 py-12 lg:p-32">
+        <div className="px-8 py-12 mx-auto max-w-screen-2xl lg:p-32 2xl:py-64">
           {children}
         </div>
         <Image
